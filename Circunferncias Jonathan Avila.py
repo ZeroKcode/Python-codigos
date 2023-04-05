265d338e50e21dd16d623becf256025681f6a94b
ZeroKcode <128632240+ZeroKcode@users.noreply.github.com>
2023-04-05 14:44:33 -0400

    Add files via upload

---
+++ Circunferncias Jonathan Avila.py
@@ -0,0 +1,8 @@
+#Definimos el valor de pi
+pi = 3.141592
+#Preguntamos por el valor del radio
+radio = float(input("Inserte el radio :"))
+#Calculamos la circunferencia con la siguiente formula
+circunferencia = float(2*pi*radio)
+#Imprimimos el resultado de la formula
+print("La circunferencia es con el radio", radio, "Es de =", circunferencia)
